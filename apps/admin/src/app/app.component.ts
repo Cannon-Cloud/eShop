f2e411bc9abecab6841e4445d7227cc7250ab34c
Cannon-Cloud <clarence@cannon.cloud>
2021-08-09 15:58:38 -0700

    Setup Commit

--- apps/admin/src/app/app.component.ts
+++ apps/admin/src/app/app.component.ts
@@ -3,7 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'cannon-cloud-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'admin';
--- apps/ngstore/src/app/app.component.ts
+++ apps/ngstore/src/app/app.component.ts
@@ -1,9 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'cannon-cloud-root',
+  selector: 'eshop-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'ngstore';
