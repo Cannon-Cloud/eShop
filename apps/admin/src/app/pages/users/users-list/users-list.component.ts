0f86d26568b5731486c6b538642fed849a6d1918
Cannon-Cloud <clarence@cannon.cloud>
2021-08-12 09:30:06 -0700

    User List Update

--- apps/admin/src/app/pages/users/users-list/users-list.component.ts
+++ apps/admin/src/app/pages/users/users-list/users-list.component.ts
@@ -1,16 +1,60 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { UsersService, User } from '@cannon-cloud/users';
+import { ConfirmationService, MessageService } from 'primeng/api';
 
 @Component({
-  selector: 'cannon-cloud-users-list',
+  selector: 'admin-users-list',
   templateUrl: './users-list.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class UsersListComponent implements OnInit {
-
-  constructor() { }
+  users: User[] = [];
+  constructor(
+    private usersService: UsersService,
+    private confirmationService: ConfirmationService,
+    private messageService: MessageService,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
+    this._getUsers();
   }
 
+  deleteUser(userId: string) {
+    this.confirmationService.confirm({
+      message: 'Do you want to Delete this User?',
+      header: 'Delete User',
+      icon: 'pi pi-exclamation-triangle',
+      accept: () => {
+        this.usersService.deleteUser(userId).subscribe(
+          () => {
+            this._getUsers();
+            this.messageService.add({
+              severity: 'success',
+              summary: 'Success',
+              detail: 'User Successfuly Deleted',
+            });
+          },
+          (error) => {
+            this.messageService.add({
+              severity: 'error',
+              summary: 'Error',
+              detail: `Error Deleting User - ${error}`,
+            });
+          }
+        );
+      },
+    });
+  }
+
+  updateUser(userId: string) {
+    this.router.navigateByUrl(`users/form/${userId}`);
+  }
+
+  private _getUsers() {
+    this.usersService.getUsers().subscribe((user) => {
+      this.users = user;
+    });
+  }
 }
---
+++ libs/users/src/lib/services/users.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '@env/environment';
+import { User } from '../models/user';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UsersService {
+  apiURLUsers = environment.apiURL + 'users';
+  constructor(private http: HttpClient) {}
+
+  getUsers(): Observable<User[]> {
+    return this.http.get<User[]>(this.apiURLUsers);
+  }
+
+  getUser(userId: string): Observable<User> {
+    return this.http.get<User>(`${this.apiURLUsers}/${userId}`);
+  }
+
+  updateUser(user: User): Observable<User> {
+    return this.http.put<User>(`${this.apiURLUsers}/${user.id}`, user);
+  }
+
+  createUser(user: User): Observable<User> {
+    return this.http.post<User>(this.apiURLUsers, user);
+  }
+
+  deleteUser(userId: string): Observable<any> {
+    return this.http.delete<any>(`${this.apiURLUsers}/${userId}`);
+  }
+}
